539d9b2d58c32b10404d039ce94aec4aba0e58ec
Nine Kinnman <nk1520@ic.ac.uk>
2023-12-07 05:05:13 +0000

    adding style

--- api/app.py
+++ api/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
-from datetime import datetime
 import requests
 
 app = Flask(__name__)
@@ -13,9 +12,7 @@ def login():
     # Retrieve the Spotify username and location from the form submission
     username = request.form.get('username')
     location = request.form.get('location')
-
-    # Here, you can use the username and location as needed
-    # For example, you can store them in the session
+    
     session['spotify_username'] = username
     session['user_location'] = location
 
@@ -24,7 +21,7 @@ def login():
 
 @app.route('/events')
 def events():
-    # Fetch music events based on Spotify username and location
-    # You can use the session data (username and location) to customize the events
-    # For example, fetch top artists based on the Spotify username and search for events in the specified location
     return render_template('events.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
