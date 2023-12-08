28d320542d4bc3147cf0153155b65b6623947b4d
Lisa Chen <lisachen011299@gmail.com>
2023-12-08 16:32:24 +0000

    flake8

--- api/utils/ticketmaster_api.py
+++ api/utils/ticketmaster_api.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 from api.utils.credentials import TICKETMASTER_APIKEY
 import requests
-import csv
+# import csv
 
 
 # app = Flask(__name__)
